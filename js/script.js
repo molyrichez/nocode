36d5ced37375e12b36c665cdeb87710a6052c598
Moly <64364914+molyrichez@users.noreply.github.com>
2022-03-16 14:44:23 +0100

    publi1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1280},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-devis","displayName":"devis","link":{"linkType":"LinkTypePage","href":"#!page-devis"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1278},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-devis","displayName":"devis","link":{"linkType":"LinkTypePage","href":"#!page-devis"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
